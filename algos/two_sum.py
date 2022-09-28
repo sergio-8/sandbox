7a67a80324617773dccda330f6d042096a1ba10f
sergio <sergiovillani8@gmail.com>
2022-09-28 16:44:22 -0700

    twak two_sum

--- algos/two_sum.py
+++ algos/two_sum.py
@@ -1,6 +1,6 @@
 from typing import List
-arr=[1,2,3,5,7,8,9,]
-t=13
+arr=[2,3,5,7,8,1,15,]
+t=16
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
@@ -13,15 +13,17 @@ class Solution:
             mappa[nums[x]]=x
             x += 1
         print(mappa)
+        print(mappa.keys())
+        print(mappa.values())
 
         x = 0
 
         while x< len(nums) and not eureka:
             tassello= target-nums[x]
-            print(tassello,'=',target, ' meno ', nums[x])
+            print(tassello,'dato da',target, ' meno ', nums[x])
 
             if tassello in mappa:
-                print('indice=', x,'valore?', mappa[tassello])
+                print('primo indice=', x,'secondo indice', mappa[tassello])
                 if x != mappa[tassello]:
 
                     paio.append(x)
