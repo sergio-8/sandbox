fdf85d94f4e298248fd0734db5ae9e40a899e95c
sergio <sergiovillani8@gmail.com>
2022-10-25 07:13:12 -0700

    refining sort

--- algos/dutch_flag.py
+++ algos/dutch_flag.py
@@ -6,4 +6,31 @@ print(list)
 
 def dutch_flag_partition(pivot_index:int, A:List[int])-> None:
     pivot = A[pivot_index]
-    for x in range (len(A))
+    print(pivot)
+    r = 0
+    for x in range (len(A)):
+
+
+        for y in range (x+1, len(A)):
+            print('evaluating round # ', r)
+            r=+1
+
+            if A[y]<pivot:
+                print('about to enforce swap round', r)
+                A[x], A[y] =A[y], A[x]
+                print('swapped A =', A)
+                
+                break
+
+    for z in reversed (range (len(A))):
+        for zap in  reversed (range (z)):
+            if A[zap]> pivot:
+                A[z], A[zap]= A[zap], A[z]
+                break
+
+
+
+    print (A)
+
+
+dutch_flag_partition(10, list)
