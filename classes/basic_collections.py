51dc32f00d447df030353dc9558c07dbca78de79
sergio <sergiovillani8@gmail.com>
2022-11-01 09:49:01 -0700

    one more basic collection updates

--- classes/basic_collections.py
+++ classes/basic_collections.py
@@ -4,21 +4,10 @@ Cards= collections.namedtuple('Card', ['rank', 'suit'])
 
 
 
-
-random_c= Cards ('Q', 'spades')
-<<<<<<< Updated upstream
-#print(random_c)
-
-class Mazzo_di_mazzi :
-    rank=[str(n) for n in range(2,11)] + list ('JOKA')
-    suit='spades diamonds clubs hearts'.split()
-=======
-print(random_c)
-
 class Mazzo_di_mazzi :
     rank=[str(n) for n in range(2,11)] + list ('JOKA')
     suit='pades diamonds clubs hearts'.split()
->>>>>>> Stashed changes
+
 
     def __init__(self):
         self._cards=[Cards(rank, suit) for rank in self.rank  for suit in self.suit]
@@ -32,9 +21,10 @@ class Mazzo_di_mazzi :
 
 
 
-<<<<<<< Updated upstream
+
 m=Mazzo_di_mazzi()
 print(len(m))
-print(m[1])
-=======
->>>>>>> Stashed changes
+print(m[-11])
+
+
+
