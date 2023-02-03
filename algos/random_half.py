a6269cb7a04a5a28aac2d6f657e90fd6308061b4
sergio-8 <sergiovillani8@gmail.com>
2023-02-02 19:13:10 -0800

    resuming

---
+++ algos/random_half.py
@@ -0,0 +1,19 @@
+import random
+
+def select_random_half_samples(data):
+    half_size = len(data) // 2
+    random_indices = random.sample(range(len(data)), half_size)
+    return [data[i] for i in random_indices]
+
+data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(select_random_half_samples(data))
+
+
+
+
+
+
+
+
+
+
