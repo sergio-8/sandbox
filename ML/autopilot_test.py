b48e7f1d6902321fdb9edc80f7846d11caee5448
sergio-8 <sergiovillani8@gmail.com>
2023-02-28 17:52:36 -0800

    resuming

---
+++ ML/autopilot_test.py
@@ -0,0 +1,20 @@
+import datetime
+import pandas as pd
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+import seaborn as sns
+import math
+import os
+import sys
+import time
+
+
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import classification_report
+
+
--- ML/nlp.py
+++ ML/nlp.py
@@ -12,6 +12,15 @@ from nltk.corpus import twitter_samples    # sample Twitter dataset from NLTK
 import matplotlib.pyplot as plt            # library for visualization
 import random                              # pseudo-random number generator
 
+
+import re                                  # library for regular expression operations
+import string                              # for string operations
+
+from nltk.corpus import stopwords          # module for stop words that come with NLTK
+from nltk.stem import PorterStemmer        # module for stemming
+from nltk.tokenize import TweetTokenizer   # module for tokenizing strings
+
+
 ssl._create_default_https_context = ssl._create_unverified_context
 #nltk.download('all')
 
@@ -29,3 +38,69 @@ print('Number of negative tweets: ', len(all_negative_tweets))
 
 print('\nThe type of all_positive_tweets is: ', type(all_positive_tweets))
 print('The type of a tweet entry is: ', type(all_negative_tweets[0]))
+print('example: ', all_negative_tweets[1])
+
+
+# Declare a figure with a custom size
+fig = plt.figure(figsize=(10, 10))
+
+# labels for the two classes
+labels = 'Positives', 'Negative'
+
+# Sizes for each slide
+sizes = [len(all_positive_tweets), len(all_negative_tweets)]
+
+# Declare pie chart, where the slices will be ordered and plotted counter-clockwise:
+plt.pie(sizes, labels=labels, autopct='%1.1f%%',
+        shadow=False, startangle=90)
+
+# Equal aspect ratio ensures that pie is drawn as a circle.
+plt.axis('equal')
+
+# Display the chart
+#plt.show()
+
+# print positive in greeen
+print('\033[92m' + all_positive_tweets[random.randint(0,5000)])
+
+# print negative in red
+print('\033[91m' + all_negative_tweets[random.randint(0,5000)])
+
+# Our selected sample. Complex enough to exemplify each step
+tweet = all_positive_tweets[2277]
+print(tweet)
+
+# download the stopwords from NLTK
+nltk.download('stopwords')
+
+
+print('\033[92m' + tweet)
+print('\033[94m')
+
+# remove old style retweet text "RT"
+tweet2 = re.sub(r'^RT[\s]+', '', tweet)
+
+# remove hyperlinks
+tweet2 = re.sub(r'https?://[^\s\n\r]+', '', tweet2)
+
+# remove hashtags
+# only removing the hash # sign from the word
+tweet2 = re.sub(r'#', '', tweet2)
+
+print(tweet2)
+
+
+print()
+print('\033[92m' + tweet2)
+print('\033[94m')
+
+# instantiate tokenizer class
+tokenizer = TweetTokenizer(preserve_case=False, strip_handles=True,
+                               reduce_len=True)
+
+# tokenize tweets
+tweet_tokens = tokenizer.tokenize(tweet2)
+
+print()
+print('Tokenized string:')
+print(tweet_tokens)
