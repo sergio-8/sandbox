f18137deca119e9e76d302c0680dd58f7fc8eb02
sergio <sergio@mac.lan>
2025-04-09 22:39:14 -0700

    Add agent.py

--- llms/agents/test_adk/agent.py
+++ llms/agents/test_adk/agent.py
@@ -2,7 +2,9 @@ from google.adk.agents import Agent
 
 root_agent = Agent(
 name="my_new_agent",
-model="gemini-2.0-pro-001",
+model="gemini-2.0-flash",
 description="A base agent.",
 instruction="You are a helpful assistant.",
 )
+
+#Agent.ListModels()
