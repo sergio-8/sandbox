9540f621b08441c099864bd582834dba41cb0783
sergio-8 <sergiovillani8@gmail.com>
2025-05-14 21:38:03 -0700

    random test

---
+++ llms/dos_mas.py
@@ -0,0 +1,12 @@
+import random
+
+# Create a random set of 10 integers
+numbers = random.sample(range(1, 100), 10)
+print(numbers)
+# Sort the numbers
+numbers.sort()
+
+# Print the sorted numbers
+print(numbers)
+
+
