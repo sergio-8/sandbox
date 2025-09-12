337b423e4268b37813cc2c0ac2aac1a8beff6821
sergio-8 <sergiovillani@google.com>
2025-09-12 18:08:28 +0000

    test agent

--- llms/agents/test_adk/agent.py
+++ llms/agents/test_adk/agent.py
@@ -4,7 +4,7 @@ root_agent = Agent(
 name="my_new_agent",
 model="gemini-2.0-flash",
 description="A base agent.",
-instruction="You are a helpful assistant.",
+instruction="You are a helpful assistant. provide detailed information. If you are not completely sure of you answer say so. without pretending to know for sure ",
 )
 
 #Agent.ListModels()
