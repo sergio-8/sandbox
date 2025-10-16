40e94b25710735ae3dc1d504c1b0ac9077a4ee7c
sergio-8 <sergiovillani8@gmail.com>
2025-10-15 20:04:10 -0700

    Requirements and utils file update

--- llms/agents/anthropic/anthropic_test_agent_1.py
+++ llms/agents/anthropic/anthropic_test_agent_1.py
@@ -3,4 +3,10 @@ import re
 import json
 
 # Local helper module
-import utils
+import utils_1
+# Use this utils.py function to load the data into a dataframe
+df = utils_1.load_and_prepare_data('coffee_sales.csv')
+
+# Grab a random sample to display
+utils_1.print_html(df.sample(n=5), title="Random Sample of Coffee Sales Data")
+
