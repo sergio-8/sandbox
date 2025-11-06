6c684e6833f17c342f934cc479bb79ab3d95319d
sergio-8 <sergiovillani8@gmail.com>
2025-11-05 20:00:12 -0800

    correction

--- llms/agents/agents_dev_space/demos/demo1/agentic_modules_1/sub_agents/safety_agent/__init__.py
+++
@@ -1,8 +0,0 @@
-# demos/demo1/agentic_modules/sub_agents/safety_agent/__init__.py
-
-# Import the agent's properties and the agent instance itself from agent.py
-from .agent import name, description, tools, safety_agent
-
-# The ADK framework expects an `invoke` function at the module level.
-# We can expose the `invoke` method of our `Agent` instance to fulfill this.
-
