9f8644d035a51c2aac5b0f3fcedb263b4515f16b
sergio-8 <sergiovillani8@gmail.com>
2026-05-21 04:24:36 +0000

    two pointers test update

--- algos/two_pointers.py
+++ algos/two_pointers.py
@@ -1,19 +1,26 @@
+import random
+
 def trovalo(arr1, target):
-  alpha=0
-  omega=len(arr1)-1
+    alpha = 0
+    omega = len(arr1) - 1
 
-  arr1.sort()
+    arr1.sort()
 
-  while alpha<omega:
-    if arr1[alpha]+arr1[omega]==target:
-      return (alpha, omega, f"which means: {arr1[alpha]} + {arr1[omega]}")
-    elif arr1[alpha]+arr1[omega]<target:
-      alpha+=1
-    else:
-      omega-=1
-  return None
+    while alpha < omega:
+        if arr1[alpha] + arr1[omega] == target:
+            print(f'Position {alpha} and position {omega}', f"which means: {arr1[alpha]} + {arr1[omega]}, results in {target}")
+            print (arr1)
+            alpha +=1 
+            omega -=1
+        elif arr1[alpha] + arr1[omega] < target:
+            alpha += 1
+        else:
+            omega -= 1
+    return None
 
-print(trovalo([1,2,3,12,9,8,4,14,5],7))
 
+# Generate 10 random positive integers between 1 and 20
+random_arr = [random.randint(1, 10) for _ in range(10)]
+print(f"Initial unsorted list: {random_arr}")
 
-    
+trovalo(random_arr, 15)
